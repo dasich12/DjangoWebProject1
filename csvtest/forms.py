ac4521a19f4ebfcaea9ca8f41acf8a2196677e0b
dasich12 <dasich12@gmail.com>
2016-08-24 11:27:52 +0700

    Revert "csvtest commit"

--- csvtest/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class codeUploadForm(forms.Form):
-    FileTitle    = forms.CharField(label='Название:', max_length=50)
-    Uploadedfile = forms.FileField(label='Файл:')
-    
